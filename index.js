21a588e6352d125fbd045bede3eac8673716aed2
Sarah Hopp <sarahhopp717@gmail.com>
2026-02-15 18:31:09 -0700

    created object weee

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-let iceCream = prompt("Enter a list of comma separated froyo flavors");
+let result = prompt("Enter a list of comma separated froyo flavors");
+console.log("User entered: " + result);
+
+//
+//
+//
+
+let flavorsArray = result.split(",");
+console.log(result.split(","));
+
+//
+//
+//
+let flavorCount = {};
